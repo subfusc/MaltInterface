583d1e85c9a3e624ebb0d535c159f8157ffd478d
Sindre <sindre.w@gmail.com>
2012-10-03 00:23:35 +0200

    improving install script

--- src/install_malt.sh
+++ src/install_malt.sh
@@ -2,38 +2,76 @@
 VERSION_NUMBER=1.7.2
 PACKAGE=maltparser-${VERSION_NUMBER}.tar.gz
 
-wget http://maltparser.org/dist/${PACKAGE}
-tar -xf ${PACKAGE}
-cd $(basename ${PACKAGE} .tar.gz)
-ant jar
-if [ -d ~/.local/share/malt/ ]; then
-    rm -rfv ~/.local/share/malt/
-fi
-mkdir -p ~/.local/share/malt/
-cp -Rfv dist/$(basename ${PACKAGE} .tar.gz)/* ~/.local/share/malt/
+EVAL_VERSION_NUMBER=1.8
+
+function install_malt() {
+    
+    wget http://maltparser.org/dist/${PACKAGE}
+    tar -xf ${PACKAGE}
+    cd $(basename ${PACKAGE} .tar.gz)
+    ant jar
+    if [ -d ~/.local/share/malt/ ]; then
+        rm -rfv ~/.local/share/malt/
+    fi
+    mkdir -p ~/.local/share/malt/
+    cp -Rfv dist/$(basename ${PACKAGE} .tar.gz)/* ~/.local/share/malt/
 #if [ -z "$(grep -o 'alias malt=' ~/.bashrc)" ]; then
 #    echo "alias malt='java -jar ~/.local/share/malt/maltparser-${VERSION_NUMBER}.jar '" >> ~/.bashrc
 #fi
 #source ~/.bashrc
-cd ..
+    cd ..
+    
+    mkdir -p ~/.local/bin/
+    if [ -f ~/.local/bin/ ]; then
+        rm ~/.local/bin/malt
+    fi
+    cat > ~/.local/bin/malt <<EOF
+#!/bin/bash
+java -jar ~/.local/share/malt/maltparser-1.7.2.jar \$@
+EOF
+    
+    if [ $(basename $(hostname) uio.no) == $(hostname) ]; then
+        echo "Make file executable, need sudo account"
+        sudo chmod u+x ~/.local/bin/malt
+    else
+        chmod u+x ~/.local/bin/malt
+    fi
+    
+    if [ -z "$(grep -o -P '^export PATH=.*\.local/bin.*$' ~/.bashrc)" ]; then
+        echo "export PATH=${HOME}/.local/bin/:${PATH}" >> ~/.bashrc
+    fi 
+    source ~/.bashrc  
 
-mkdir -p ~/.local/bin/
-if [ -f ~/.local/bin/ ]; then
-    rm ~/.local/bin/malt
-fi
-cp malt ~/.local/bin/malt
+    rm -rf $(basename ${PACKAGE} .tar.gz)
+    rm ${PACKAGE}
+}
 
-if [ $(basename $(hostname) uio.no) == $(hostname) ]; then
-    echo "Make file executable, need sudo account"
-    sudo chmod u+x ~/.local/bin/malt
-else
-    chmod u+x ~/.local/bin/malt
-fi
+function install_eval() {
+    wget http://ilk.uvt.nl/conll/software/eval.pl
+    if [ -d ~/.local/share/conll-eval/ ]; then
+        rm -rfv ~/.local/share/conll-eval/
+    fi
+    mkdir -p ~/.local/share/conll-eval/
+    mv eval.pl ~/.local/share/conll-eval/
 
-if [ -z "$(grep -o -P '^export PATH=.*\.local/bin.*$' ~/.bashrc)" ]; then
-    echo "export PATH=${HOME}/.local/bin/:${PATH}" >> ~/.bashrc
-fi 
-source ~/.bashrc  
+    cat > ~/.local/bin/conll-eval <<EOF
+#!/bin/bash
+perl ~/.local/share/conll-eval/eval.pl \$@
+EOF
+    
+    if [ -z "$(grep -o -P '^export PATH=.*\.local/bin.*$' ~/.bashrc)" ]; then
+        echo "export PATH=${HOME}/.local/bin/:${PATH}" >> ~/.bashrc
+    fi 
+    source ~/.bashrc  
 
-rm -rf $(basename ${PACKAGE} .tar.gz)
-rm ${PACKAGE}
+    if [ $(basename $(hostname) uio.no) == $(hostname) ]; then
+        echo "Make file executable, need sudo account"
+        sudo chmod u+x ~/.local/bin/conll-eval
+    else
+        chmod u+x ~/.local/bin/conll-eval
+    fi
+
+}
+
+install_malt
+install_eval
